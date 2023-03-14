7cb36676d6724d75d4207ad563d4f4debf1fdd97
Brandon Wise <220049173@mycput.ac.za>
2023-03-14 16:32:11 +0200

    Employee POJO completed. 3/5 test methods in TestEmployee class completed.

--- src/main/java/za/ac/cput/domain/Employee.java
+++ src/main/java/za/ac/cput/domain/Employee.java
@@ -1,7 +1,67 @@
 package za.ac.cput.domain;
 
 public class Employee {
-    public static void main(String[] args) {
+        private String employeeNumber;
+        private String firstName;
+        private String lastName;
 
+    public Employee(){}
+
+    public Employee(String employeeNumber, String firstName, String lastName){
+
+        if (employeeNumber == null || employeeNumber.isEmpty()) {
+            throw new IllegalArgumentException("Employee number cannot be null or empty.");
+        }
+        if (firstName == null || firstName.isEmpty()) {
+            throw new NullPointerException("First name cannot be null or empty.");
+        }
+        if (lastName == null || lastName.isEmpty()) {
+            throw new NullPointerException("Last name cannot be null or empty.");
+        }
+
+        this.employeeNumber = employeeNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeNumber() { return employeeNumber; }
+
+    public String getFirstName() { return firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) obj;
+        return this.employeeNumber.equals(other.employeeNumber)
+                && this.firstName.equals(other.firstName)
+                && this.lastName.equals(other.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeNumber='" + employeeNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
